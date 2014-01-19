62a27006a747ae47c3c6993f8fd787bec086fc9a
▟ ▖▟ ▖ <dodo.the.last@gmail.com>
2014-01-19 20:54:20 +0100

    add working example

--- xmpp.js
+++ xmpp.js
@@ -5,4 +5,4 @@ require('util').debuglog = function (tag) {
 var xmpp = require('node-xmpp');
 if (window) window.xmpp = xmpp;
 
-// var client = new xmpp.Client({jid:"jid@domain.lit", password:"secret"});
+// var client = new xmpp.Client({jid:"jid@domain.lit", password:"secret", host:"domain.lit", preferred:"PLAIN"})
